0d3055a3a1dc3e6203bca80ddffc83d6a9488e0c
Huemman <mehmehkyuto@gmail.com>
2023-05-29 07:19:01 -0800

    added files to gitignore

--- config.py
+++ config.py
@@ -8,12 +8,11 @@ credentials = {
     'host': 'localhost'
 }
 
-try:
-    mysql = MySQL()
-    app.config['MYSQL_DATABASE_USER'] = credentials['username']
-    app.config['MYSQL_DATABASE_PASSWORD'] = credentials['pass']
-    app.config['MYSQL_DATABASE_DB'] = credentials['db']
-    app.config['MYSQL_DATABASE_HOST'] = credentials['host']
-    mysql.init_app(app) 
-except Exception as e: 
-    print(e)
+
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = credentials['username']
+app.config['MYSQL_DATABASE_PASSWORD'] = credentials['pass']
+app.config['MYSQL_DATABASE_DB'] = credentials['db']
+app.config['MYSQL_DATABASE_HOST'] = credentials['host']
+mysql.init_app(app) 
+
